1c342d8e28392c6280e47445eeaf2988ecdb3526
Raphael Bernhard <beraphae@gmail.com>
2024-12-01 14:44:29 +0100

    reformat

--- store/effect.ts
+++ store/effect.ts
@@ -5,100 +5,100 @@ export type Destructor = () => void;
 export type EffectCallback = () => void | Destructor;
 
 class DependencyTracker {
-    dependencies: Set<Store<unknown>> = new Set();
+	dependencies: Set<Store<unknown>> = new Set();
 
-    register(store: Store<unknown>): void {
-        this.dependencies.add(store);
-    }
+	register(store: Store<unknown>): void {
+		this.dependencies.add(store);
+	}
 }
 
 class DependencyTrackerContext {
-    private readonly trackers = <DependencyTracker[]>[];
+	private readonly trackers = <DependencyTracker[]>[];
 
-    push(): DependencyTracker {
-        const ret = new DependencyTracker();
-        this.trackers.push(ret);
-        return ret;
-    }
+	push(): DependencyTracker {
+		const ret = new DependencyTracker();
+		this.trackers.push(ret);
+		return ret;
+	}
 
-    pop(): void {
-        this.trackers.pop();
-    }
+	pop(): void {
+		this.trackers.pop();
+	}
 
-    register(store: Store<unknown>): void {
-        if (this.trackers.length === 0) {
-            return;
-        }
-        const tracker = this.trackers[this.trackers.length - 1];
-        if (tracker) {
-            tracker.register(store);
-        }
-    }
+	register(store: Store<unknown>): void {
+		if (this.trackers.length === 0) {
+			return;
+		}
+		const tracker = this.trackers[this.trackers.length - 1];
+		if (tracker) {
+			tracker.register(store);
+		}
+	}
 }
 
-
-export const dependencyTrackerContext: DependencyTrackerContext = new DependencyTrackerContext();
+export const dependencyTrackerContext: DependencyTrackerContext =
+	new DependencyTrackerContext();
 
 export class Effect {
-    private forzen = false;
-    private markedForRun = false;
-    private unsubscribers = <Unsubscriber[]>[];
-    private cleanup: Destructor | undefined;
+	private forzen = false;
+	private markedForRun = false;
+	private unsubscribers = <Unsubscriber[]>[];
+	private cleanup: Destructor | undefined;
 
-    constructor(private readonly fn: EffectCallback) {}
+	constructor(private readonly fn: EffectCallback) {}
 
-    private onChanged() {
-        if (this.forzen) {
-            this.markedForRun = true;
-        } else {
-            this.run();
-        }
-    }
+	private onChanged() {
+		if (this.forzen) {
+			this.markedForRun = true;
+		} else {
+			this.run();
+		}
+	}
 
-    freeze(): void {
-        this.forzen = true;
-    }
+	freeze(): void {
+		this.forzen = true;
+	}
 
-    thaw(): void {
-        this.forzen = false;
-        if (this.markedForRun) {
-            this.markedForRun = false;
-            this.run();
-        }
-    }
+	thaw(): void {
+		this.forzen = false;
+		if (this.markedForRun) {
+			this.markedForRun = false;
+			this.run();
+		}
+	}
 
-    run(): void {
-        if (this.cleanup) {
-            this.cleanup();
-            this.cleanup = undefined;
-        }
-        this.unsubscribe();
-        const tracker = dependencyTrackerContext.push();
-        try {
-            const cleanup = this.fn();
-            if (cleanup) {
-                this.cleanup = cleanup;
-            }
-            const deps = tracker.dependencies;
-            for (const dep of deps) {
-                const unsub = dep.subscribe(() => this.onChanged());
-                this.unsubscribers.push(unsub);
-            }
-            this.markedForRun = false;
-        } finally {
-            dependencyTrackerContext.pop();
-        }
-    }
+	run(): void {
+		if (this.cleanup) {
+			this.cleanup();
+			this.cleanup = undefined;
+		}
+		this.unsubscribe();
+		const tracker = dependencyTrackerContext.push();
+		try {
+			const cleanup = this.fn();
+			if (cleanup) {
+				this.cleanup = cleanup;
+			}
+			const deps = tracker.dependencies;
+			for (const dep of deps) {
+				const unsub = dep.subscribe(() => this.onChanged());
+				this.unsubscribers.push(unsub);
+			}
+			this.markedForRun = false;
+		} finally {
+			dependencyTrackerContext.pop();
+		}
+	}
 
-    unsubscribe(): void {
-        for (const unsub of this.unsubscribers) {
-            unsub();
-        }
-    }
+	unsubscribe(): void {
+		for (const unsub of this.unsubscribers) {
+			unsub();
+		}
+	}
 }
 
 export const effect = (fn: EffectCallback): Effect => {
-    const eff = new Effect(fn);
-    eff.run();
-    return eff;
+	const eff = new Effect(fn);
+	eff.run();
+	return eff;
 };
--- store/store.ts
+++ store/store.ts
@@ -7,83 +7,86 @@ export type Subscriber = () => void;
 export type Unsubscriber = () => void;
 
 export interface Store<T> {
-    subscribe(subscriber: Subscriber): Unsubscriber;
-    get(): T;
+	subscribe(subscriber: Subscriber): Unsubscriber;
+	get(): T;
 }
 
 export interface Write<T> {
-    set(newState: T): void;
+	set(newState: T): void;
 }
 
 export interface Update<T> {
-    update(fn: (state: T) => void): void;
+	update(fn: (state: T) => void): void;
 }
 
-export const store: <T>(initalState: T) => Store<T> & Write<T> & Update<T> = <T>(
-    initialState: T
+export const store: <T>(initalState: T) => Store<T> & Write<T> & Update<T> = <
+	T,
+>(
+	initialState: T,
 ) => {
-    return new StoreImpl(initialState);
+	return new StoreImpl(initialState);
 };
 
 export type UpdateableStore<T> = Store<T> & Update<T>;
 
 export type WritableStore<T> = Store<T> & Write<T> & Update<T>;
 
-
-export function isUpdateable<T>(store: Store<T> | Store<T> & Write<T>): store is Store<T> & Update<T> {
-    return (store as Store<T> & Update<T>).update !== undefined;
+export function isUpdateable<T>(
+	store: Store<T> | (Store<T> & Write<T>),
+): store is Store<T> & Update<T> {
+	return (store as Store<T> & Update<T>).update !== undefined;
 }
 
 class StoreImpl<T> implements Store<T>, Write<T>, Update<T> {
-    private _state: T;
-    private subscribers: Set<Subscriber>;
-    private _recursiveId = 0;
+	private _state: T;
+	private subscribers: Set<Subscriber>;
+	private _recursiveId = 0;
 
-    constructor(initialState: T) {
-        this._state = initialState;
-        this.subscribers = new Set();
-    }
+	constructor(initialState: T) {
+		this._state = initialState;
+		this.subscribers = new Set();
+	}
 
-    get(): T {
-        dependencyTrackerContext.register(this);
-        return this._state;
-    }
+	get(): T {
+		dependencyTrackerContext.register(this);
+		return this._state;
+	}
 
-    set(newState: T): void {
-        this._state = newState;
-        this.flush();
-    }
+	set(newState: T): void {
+		this._state = newState;
+		this.flush();
+	}
 
-    update(fn: (state: T) => void): void {
-        const opts: ExternalOptions<false, false> = {
-            mark: (target) => {
-                if (target instanceof StoreImpl) {
-                    return () => target;
-                }
-                if (target instanceof UpdateableLens) {
-                    return () => target;
-                }
-            },
-        };
-        this._state = create(this._state, fn, opts);
-        this.flush();
-    }
+	update(fn: (state: T) => void): void {
+		const opts: ExternalOptions<false, false> = {
+			mark: (target) => {
+				if (target instanceof StoreImpl) {
+					return () => target;
+				}
+				if (target instanceof UpdateableLens) {
+					return () => target;
+				}
+			},
+		};
+		this._state = create(this._state, fn, opts);
+		this.flush();
+	}
 
-    subscribe(subscriber: Subscriber): Unsubscriber {
-        this.subscribers.add(subscriber);
-        return () => {
-            this.subscribers.delete(subscriber);
-        };
-    }
+	subscribe(subscriber: Subscriber): Unsubscriber {
+		this.subscribers.add(subscriber);
+		return () => {
+			this.subscribers.delete(subscriber);
+		};
+	}
 
-    private flush() {
-        this._recursiveId++;
-        const recursiveId = this._recursiveId;
-        for (const subscriber of this.subscribers) {
-            if (recursiveId !== this._recursiveId) {
-                return;
-            }
-            subscriber();
-        }
-    }
+	private flush() {
+		this._recursiveId++;
+		const recursiveId = this._recursiveId;
+		for (const subscriber of this.subscribers) {
+			if (recursiveId !== this._recursiveId) {
+				return;
+			}
+			subscriber();
+		}
+	}
 }
